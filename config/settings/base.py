2a08848e82111d09ec4d4e5fbe239e1d01bc9654
Stanislav Valasek <valasek@gmail.com>
2021-11-18 23:06:56 +0100

    Update to the latest packages

--- config/settings/base.py
+++ config/settings/base.py
@@ -300,3 +300,8 @@ SOCIALACCOUNT_ADAPTER = "kicoma.users.adapters.SocialAccountAdapter"
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
